da15f5824210bf12289c4d30dc6a96e3f2c2334e
Micah Banschick <micha.ban@gmail.com>
2020-01-22 13:51:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,5 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma
-  if 
+def speak_to_grandma(string)
+  if string != string.upcase 
+    return "HUH?! SPEAK UP, SONNY!"
+    elsif string == 
