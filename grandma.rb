b3e53f7b1e097d6c230c0417566672ed622f62fc
Micah Banschick <micha.ban@gmail.com>
2020-01-22 13:52:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,3 +16,6 @@ def speak_to_grandma(string)
   if string != string.upcase 
     return "HUH?! SPEAK UP, SONNY!"
     elsif string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else 
+    return 
